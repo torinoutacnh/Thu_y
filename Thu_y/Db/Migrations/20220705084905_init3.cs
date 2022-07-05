4bc5af82973a7b92482966ece25a145e77b91758
trân hòa hiệp <hiep_big_boy@outlook.com>
2022-07-05 15:51:08 +0700

    update

---
+++ Thu_y/Db/Migrations/20220705084905_init3.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Thu_y.Db.Migrations
+{
+    public partial class init3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Receipt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Receipt",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
