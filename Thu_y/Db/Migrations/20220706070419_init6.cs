37a97d48eae65d6c8b1b8fae216ed93d6c71a980
tori <torinoutacnh@gmail.com>
2022-07-06 14:13:19 +0700

    Revert "Revert "asd""
    This reverts commit 593a8491fbe8ca26cf6b04af69601d7e56557092.

---
+++ Thu_y/Db/Migrations/20220706070419_init6.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Thu_y.Db.Migrations
+{
+    public partial class init6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SortNo",
+                table: "FormAttribute",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SortNo",
+                table: "FormAttribute");
+        }
+    }
+}
