daba26a323dd79570eedf8f3045ade3e4b8b692f
trân hòa hiệp <hiep_big_boy@outlook.com>
2022-07-08 02:55:01 +0700

    migrate

---
+++ Thu_y/Db/Migrations/20220707194513_init10.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Thu_y.Db.Migrations
+{
+    public partial class init10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ListAnimalEntity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AnimalName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnimalId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsCar = table.Column<bool>(type: "bit", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    AnimalSex = table.Column<int>(type: "int", nullable: false),
+                    Purpose = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReportTicketId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DateUpdated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DateDeleted = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListAnimalEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ListAnimalEntity_ReportTicket_ReportTicketId",
+                        column: x => x.ReportTicketId,
+                        principalTable: "ReportTicket",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SealTabEntity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CodeSeal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReportTicketId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DateUpdated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DateDeleted = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SealTabEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SealTabEntity_ReportTicket_ReportTicketId",
+                        column: x => x.ReportTicketId,
+                        principalTable: "ReportTicket",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ListAnimalEntity_ReportTicketId",
+                table: "ListAnimalEntity",
+                column: "ReportTicketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SealTabEntity_ReportTicketId",
+                table: "SealTabEntity",
+                column: "ReportTicketId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ListAnimalEntity");
+
+            migrationBuilder.DropTable(
+                name: "SealTabEntity");
+        }
+    }
+}
