0754794c983a47fae78be0a55012643f3a069319
trân hòa hiệp <hiep_big_boy@outlook.com>
2022-07-08 00:41:33 +0700

    update

--- Thu_y/Modules/UserModule/Model/UserDtoModel.cs
+++ Thu_y/Modules/UserModule/Model/UserDtoModel.cs
@@ -2,7 +2,7 @@
 {
     public class UserDtoModel
     {
-        public string UsetName { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
 
     }
