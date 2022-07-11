c37a5a2b6e54fea08c13a88bd8721bc27b093c62
trân hòa hiệp <hiep_big_boy@outlook.com>
2022-07-11 16:36:07 +0700

    update

--- Thu_y/Modules/ReportModule/Endpoints/ReportRoute.cs
+++ Thu_y/Modules/ReportModule/Endpoints/ReportRoute.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Thu_y.Modules.ReportModule.Model;
