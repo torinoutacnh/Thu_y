245c2a2ad7ce71f79856317d02426178f7d8d1a3
Hiepth <thanhtrunglett@gmail.com>
2022-07-28 21:39:40 +0700

    update

---
+++ Thu_y/Db/Migrations/20220728062108_init28.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Thu_y.Db.Migrations
+{
+    public partial class init28 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RemainPage",
+                table: "ReceiptAllocate",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RemainPage",
+                table: "ReceiptAllocate");
+        }
+    }
+}
