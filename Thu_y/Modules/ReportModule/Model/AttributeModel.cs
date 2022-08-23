4abf9cdb22bd98c43db713809df01e176f3d99de
torinoutacnh <54936986+torinoutacnh@users.noreply.github.com>
2022-08-23 20:51:01 +0700

    a

--- Thu_y/Modules/ReportModule/Model/AttributeModel.cs
+++ Thu_y/Modules/ReportModule/Model/AttributeModel.cs
@@ -8,6 +8,7 @@ namespace Thu_y.Modules.ReportModule.Model
         public string Name { get; set; }
         public DataTypes DataType { get; set; }
         public ControlTypes ControlType { get; set; }
+        public string? api_DropDownlist { get; set; }
         public int SortNo { get; set; }
         public string AttributeCode { get; set; }
         public string Value { get; set; }
